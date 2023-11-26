42c78560b00564ed07e078429c183794354f858b
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-11-26 17:43:46 +0200

    update

--- app/js/main.js
+++ app/js/main.js
@@ -10,9 +10,27 @@ const swiper = new Swiper(".mySwiper", {
     },
 });
 
-function showCalendar() {
-    var dateInput = document.getElementById("myDate");
-    var calendar = new Date();
-    var picker = new Calendar(dateInput, calendar);
-    picker.show();
-}
+// questions
+
+
+
+const questionsList = document.querySelectorAll('.questions__list');
+
+questionsList.forEach((listItem) => {
+    const listItemHeader = listItem.querySelector('.questions__list_header');
+    const listItemBody = listItem.querySelector('.questions__list_body');
+
+    listItemHeader.addEventListener('click', () => {
+        const isActive = listItem.classList.contains('questions__list_active');
+
+        // Відкриваємо або закриваємо поточне питання
+        if (!isActive) {
+            listItem.classList.add('questions__list_active');
+            listItemBody.classList.add('questions__body_active');
+        } else {
+            listItem.classList.remove('questions__list_active');
+            listItemBody.classList.remove('questions__body_active');
+        }
+    });
+});
+
